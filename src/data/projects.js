8b6eb49f15ca15890648b937fcbe4e61d386e726
Daniel Jimenez <142844473+RedRobin995@users.noreply.github.com>
2026-03-15 18:21:31 -0600

    Projects update

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,4 +1,5 @@
 import pokedexImage from "../assets/pokedex-preview.png"
+import mtgImage from "../assets/MTG preview img.png"
 
 export const projects = [
   {
@@ -9,4 +10,15 @@ export const projects = [
     link: "https://redrobin995.github.io/pokedex-project/",
     image: pokedexImage
   },
+
+  {
+    title: "Trading Card Game Store",
+    description:
+      "Responsive web for Trading Card Game Store that pulls data from the Scryfall API.",
+    tech: ["React", "HTML", "CSS", "JavaScript", "Scryfall API"],
+    link: "https://redrobin995.github.io/MTG-STORE-REACT-PROYECT-2026/",
+    image: mtgImage
+  },
+
+
 ]
